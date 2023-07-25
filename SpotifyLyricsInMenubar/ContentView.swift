5a1f304908c99bbb8dec303385169ecaa0488a91
Avi Wadhwa <aviwad@gmail.com>
2023-07-26 00:11:51 +0530

    Initial Commit

---
+++ SpotifyLyricsInMenubar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyLyricsInMenubar
+//
+//  Created by Avi Wadhwa on 26/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyLyricsInMenubar/SpotifyLyricsInMenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyLyricsInMenubarApp.swift
+//  SpotifyLyricsInMenubar
+//
+//  Created by Avi Wadhwa on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyLyricsInMenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
