76ead88efaa070a73a7ced1ab988dfb47f0446ff
Avi Wadhwa <aviwad@gmail.com>
2023-08-01 14:33:24 +0530

    fetch lyrics, use spotify distributed notifications

--- SpotifyLyricsInMenubar/ContentView.swift
+++ SpotifyLyricsInMenubar/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentlyPlaying: String?
+    @State var currentlyPlayingLyrics: [LyricLine] = []
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(currentlyPlaying != nil ? "Current Spotify Track ID: \(currentlyPlaying!)" : "Nothing playing")
+            List {
+                ForEach(currentlyPlayingLyrics.indices, id: \.self) { lyricIndex in
+                    HStack {
+                        Text(currentlyPlayingLyrics[lyricIndex].startTimeMS)
+                            .font(.subheadline)
+                        Text(currentlyPlayingLyrics[lyricIndex].words)
+                    }
+                }
+            }
+        }
+        .onReceive(DistributedNotificationCenter.default().publisher(for: Notification.Name(rawValue:  "com.spotify.client.PlaybackStateChanged")), perform: { notification in
+            currentlyPlaying = (notification.userInfo?["Track ID"] as? String)?.components(separatedBy: ":").last
+        })
+        .onChange(of: currentlyPlaying) { nowPlaying in
+            print(nowPlaying)
+            Task {
+                if let lyrics = await lyricsFetcher().fetchLyrics(for: nowPlaying) {
+                    currentlyPlayingLyrics = lyrics
+                }
+            }
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
+    
+    actor lyricsFetcher {
+        func fetchLyrics(for trackID: String?) async -> [LyricLine]? {
+            if let trackID, let url = URL(string: "https://spotify-lyric-api.herokuapp.com/?trackid=\(trackID)"), let urlResponseAndData = try? await URLSession.shared.data(from: url), let lyrics = try? JSONDecoder().decode(LyricJson.self, from: urlResponseAndData.0) {
+                return lyrics.lines
+            }
+            return nil
+        }
+    }
 }
--- SpotifyLyricsInMenubar/lyricJsonStruct.swift
+++ SpotifyLyricsInMenubar/lyricJsonStruct.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+// MARK: - Welcome5
+struct LyricJson: Codable {
+    let error: Bool
+    let syncType: String
+    let lines: [LyricLine]
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.error = try container.decode(Bool.self, forKey: .error)
+        self.syncType = try container.decode(String.self, forKey: .syncType)
+        self.lines = try container.decode([LyricLine].self, forKey: .lines)
+    }
+}
+
+// MARK: - Line
+struct LyricLine: Codable {
+//    let startTimeMS, words: String
+////    let syllables: [Any?]
+//    let endTimeMS: String
+//    
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.startTimeMS = try container.decode(String.self, forKey: .startTimeMS)
+//        self.words = try container.decode(String.self, forKey: .words)
+//        self.endTimeMS = try container.decode(String.self, forKey: .endTimeMS)
+//    }
+    let startTimeMS, words: String
+    let syllables: [String]
+    let endTimeMS: String
+
+    enum CodingKeys: String, CodingKey {
+        case startTimeMS = "startTimeMs"
+        case words, syllables
+        case endTimeMS = "endTimeMs"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.startTimeMS = try container.decode(String.self, forKey: .startTimeMS)
+        self.words = try container.decode(String.self, forKey: .words)
+        self.syllables = []
+        self.endTimeMS = try container.decode(String.self, forKey: .endTimeMS)
+    }
+}
