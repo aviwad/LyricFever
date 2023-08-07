cf5a32dd759f3dd19a54d2dedd61ae8239f15e16
Avi Wadhwa <aviwad@gmail.com>
2023-08-07 22:18:34 +0530

    read sync type from api, only return lyrics if line synced

--- SongObject+CoreDataClass.swift
+++ SongObject+CoreDataClass.swift
@@ -12,7 +12,7 @@ import CoreData
 @objc(SongObject)
 public class SongObject: NSManagedObject, Decodable {
     enum CodingKeys: String, CodingKey {
-        case lines
+        case lines, syncType
     }
 
     public required convenience init(from decoder: Decoder) throws {
@@ -23,15 +23,15 @@ public class SongObject: NSManagedObject, Decodable {
         self.init(context: context)
         self.id = trackID
         self.title = trackName
+        self.downloadDate = Date.now
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        if let lyrics = try? container.decode([LyricLine].self, forKey: .lines) {
+        if let syncType = try? container.decode(String.self, forKey: .syncType), syncType == "LINE_SYNCED", let lyrics = try? container.decode([LyricLine].self, forKey: .lines) {
             self.lyricsTimestamps = lyrics.map {$0.startTimeMS}
             self.lyricsWords = lyrics.map {$0.words}
         } else {
             self.lyricsWords = []
             self.lyricsTimestamps = []
         }
-        self.downloadDate = Date.now
     }
     
 }
