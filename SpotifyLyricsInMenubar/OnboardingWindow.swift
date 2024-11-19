df08d6460782be46fc57036764f41558ccf800b7
Avi Wadhwa <aviwad@gmail.com>
2024-11-18 22:24:35 -0800

    Version 2.0, enable code stripping

--- SpotifyLyricsInMenubar/OnboardingWindow.swift
+++ SpotifyLyricsInMenubar/OnboardingWindow.swift
@@ -192,7 +192,7 @@ struct OnboardingWindow: View {
                     }
                     
                     
-                    Text("Email me at [aviwad@gmail.com](mailto:aviwad@gmail.com) for any support\n⚠️ Disclaimer: I do not own the rights to Spotify or the lyric content presented.\nMusixmatch and Spotify own all rights to the lyrics.\n [Lyric Fever GitHub](https://github.com/aviwad/LyricFever)\nVersion 1.9")
+                    Text("Email me at [aviwad@gmail.com](mailto:aviwad@gmail.com) for any support\n⚠️ Disclaimer: I do not own the rights to Spotify or the lyric content presented.\nMusixmatch and Spotify own all rights to the lyrics.\n [Lyric Fever GitHub](https://github.com/aviwad/LyricFever)\nVersion 2.0")
                         .multilineTextAlignment(.center)
                         .font(.callout)
                         .padding(.top, 10)
