86e53c32666930640c56b78fed458e8737029985
Avi Wadhwa <aviwad@gmail.com>
2025-03-19 00:27:02 -0700

    add overrideprint (commented out for now)

---
+++ SpotifyLyricsInMenubar/OverridePrint.swift
@@ -0,0 +1,22 @@
+//
+//  OverridePrint.swift
+//  Lyric Fever
+//
+//  Created by Avi Wadhwa on 2025-03-08.
+//
+
+//func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+//
+//    #if DEBUG
+//
+//    var idx = items.startIndex
+//    let endIdx = items.endIndex
+//
+//    repeat {
+//        Swift.print(items[idx], separator: separator, terminator: idx == (endIdx - 1) ? terminator : separator)
+//        idx += 1
+//    }
+//    while idx < endIdx
+//
+//    #endif
+//}
