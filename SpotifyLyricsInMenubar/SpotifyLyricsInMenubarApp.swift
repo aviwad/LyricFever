3f9f7247172fcb46736b9db0d8f082b80d95ba5b
Avi Wadhwa <aviwad@gmail.com>
2025-05-08 08:00:37 -0700

    Return nil ID for Spotify URLs for podcasts

--- SpotifyLyricsInMenubar/SpotifyLyricsInMenubarApp.swift
+++ SpotifyLyricsInMenubar/SpotifyLyricsInMenubarApp.swift
@@ -701,7 +701,9 @@ extension String {
     func spotifyProcessedUrl() -> String? {
         let components = self.components(separatedBy: ":")
         guard components.count > 2 else { return nil }
-
+        if components[1] == "episode" {
+            return nil
+        }
         if components[1] == "local" {
             var localTrackId = components.dropFirst(2).dropLast().joined(separator: ":")
             // Ensures only Spotify tracks have a length of 22
