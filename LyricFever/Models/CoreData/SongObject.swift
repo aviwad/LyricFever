53b54d16a23e17ab868bfe6aa1f60b18b152644b
Avi Wadhwa <aviwad@gmail.com>
2025-08-05 21:06:35 -0700

    Rename SongObject class files, get rid of Decodable() init. We decode Spotify lyrics in a separate struct

--- LyricFever/Models/CoreData/SongObject.swift
+++ LyricFever/Models/CoreData/SongObject.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreData
 
 @objc(SongObject)
-public class SongObject: NSManagedObject, Decodable {
+public class SongObject: NSManagedObject {
     enum CodingKeys: String, CodingKey {
         case lines, language, syncType
     }
@@ -54,31 +54,31 @@ public class SongObject: NSManagedObject, Decodable {
         
     }
 
-    public required convenience init(from decoder: Decoder) throws {
-        guard let context = decoder.userInfo[CodingUserInfoKey.managedObjectContext] as? NSManagedObjectContext, let trackID = decoder.userInfo[CodingUserInfoKey.trackID] as? String, let trackName = decoder.userInfo[CodingUserInfoKey.trackName] as? String, let duration = decoder.userInfo[CodingUserInfoKey.duration] as? TimeInterval else {
-            fatalError()
-        }
-
-        self.init(context: context)
-        self.id = trackID
-        self.title = trackName
-        self.downloadDate = Date.now
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.language = (try? container.decode(String.self, forKey: .language)) ?? ""
-        if let syncType = try? container.decode(String.self, forKey: .syncType), syncType == "LINE_SYNCED", var lyrics = try? container.decode([LyricLine].self, forKey: .lines) {
-            // Dummy lyric at the end to keep the timer going past the last lyric, necessary for someone playing a single song on repeat
-            // Spotify doesn't give playback notifications when it's the same song on repeat
-            // Apple Music does, but unfortunately has every song slightly longer than it's spotify counterpart so this doesn't help us
-            lyrics.removeAll { $0.words == ""}
-            if !lyrics.isEmpty {
-                lyrics.append(LyricLine(startTime: duration+5000, words: "Now Playing: \(title)"))
-            }
-            self.lyricsTimestamps = lyrics.map {$0.startTimeMS}
-            self.lyricsWords = lyrics.map {$0.words}
-        } else {
-            self.lyricsWords = []
-            self.lyricsTimestamps = []
-        }
-    }
+//    public required convenience init(from decoder: Decoder) throws {
+////        guard let context = decoder.userInfo[CodingUserInfoKey.managedObjectContext] as? NSManagedObjectContext, let trackID = decoder.userInfo[CodingUserInfoKey.trackID] as? String, let trackName = decoder.userInfo[CodingUserInfoKey.trackName] as? String, let duration = decoder.userInfo[CodingUserInfoKey.duration] as? TimeInterval else {
+////            fatalError()
+////        }
+//
+//        self.init(context: context)
+//        self.id = trackID
+//        self.title = trackName
+//        self.downloadDate = Date.now
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.language = (try? container.decode(String.self, forKey: .language)) ?? ""
+//        if let syncType = try? container.decode(String.self, forKey: .syncType), syncType == "LINE_SYNCED", var lyrics = try? container.decode([LyricLine].self, forKey: .lines) {
+//            // Dummy lyric at the end to keep the timer going past the last lyric, necessary for someone playing a single song on repeat
+//            // Spotify doesn't give playback notifications when it's the same song on repeat
+//            // Apple Music does, but unfortunately has every song slightly longer than it's spotify counterpart so this doesn't help us
+//            lyrics.removeAll { $0.words == ""}
+//            if !lyrics.isEmpty {
+//                lyrics.append(LyricLine(startTime: duration+5000, words: "Now Playing: \(title)"))
+//            }
+//            self.lyricsTimestamps = lyrics.map {$0.startTimeMS}
+//            self.lyricsWords = lyrics.map {$0.words}
+//        } else {
+//            self.lyricsWords = []
+//            self.lyricsTimestamps = []
+//        }
+//    }
     
 }
