0c69ad1a148970154399620c16eaf2a4dbbd13e2
Avi Wadhwa <aviwad@gmail.com>
2026-03-02 20:39:15 +0530

    3.3 update window minor UI fixes

--- LyricFever/Past Updates/UpdateWindow.swift
+++ LyricFever/Past Updates/UpdateWindow.swift
@@ -10,33 +10,42 @@ import SwiftUI
 struct UpdateWindow: View {
     @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack(spacing: 25) {
+        VStack {
             HStack {
                 Image("hi")
                     .resizable()
                     .frame(width: 70, height: 70, alignment: .center)
                 
-                Text(verbatim:"Thanks for updating to Lyric Fever 3.2! 🎉")
-                    .font(.title)
+                Text(verbatim:"Thanks for updating to Lyric Fever 3.3! 🎉")
+//                    .font(.title)
+                    .font(.custom("ChalkboardSE-Regular", size: 30))
             }
             
-            Text(verbatim: "Emergency update to fix Spotify login")
-                .padding(.horizontal, 10)
+//            Text(verbatim: "Emergency update to fix Spotify login")
+//                .padding(.horizontal, 10)
             
-//            Text(verbatim:"3.0 Changes")
+            Text(verbatim:"3.3 Changes")
 //                .font(.title2)
-//                .foregroundStyle(.green)
-//            ScrollView {
+                .font(.custom("ChalkboardSE-Regular", size: 30))
+                .foregroundStyle(.green)
+            ScrollView {
+                Text(updateNotes33.map { "- " + $0 }.joined(separator: "\n"))
+                    .textSelection(.enabled)
 //                VStack(alignment: .leading, spacing: 3) {
-//                    ForEach(updateNotes23, id: \.self) { updateNote in
+//                    ForEach(updateNotes33, id: \.self) { updateNote in
 //                        Text(verbatim: "- " + updateNote)
 //                    }
 //                }
-//                .frame(alignment: .leading)
-//                .padding(.horizontal,10)
-//                .padding(.vertical,5)
-//                .background(Color(nsColor: NSColor.darkGray).cornerRadius(7))
-//            }
+//                .textSelection(.enabled)
+                .frame(alignment: .leading)
+                .padding(.horizontal,10)
+                .padding(.vertical,5)
+                .background(
+                    RoundedRectangle(cornerRadius: 7)
+                        .fill(Color(.secondarySystemFill))
+                )
+//                .background(Color.quaternarySystemFill.cornerRadius(7))
+            }
             Button("Close") {
                 dismiss()
             }
@@ -45,5 +54,6 @@ struct UpdateWindow: View {
             .buttonStyle(.borderedProminent)
             .padding(.bottom, 10)
         }
+        .padding(.horizontal, 10)
     }
 }
